8e4188a8752cbef7f4d04f1c797ae3b34b0d9b1b
tanjanita <szonoko+github@gmail.com>
2021-07-14 12:10:08 +0100

    Closes #7 Home: Clean up mobile layout

---
+++ src/Portfolio.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <article>
+
+      <h1 id="portfolio">
+      Portfolio
+      </h1>
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+      This is some main content <br />
+
+    </article>
+  );
+}
+
+export default Portfolio;
