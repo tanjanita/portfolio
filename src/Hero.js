e9223540d2e96e65f04601134bf5a3984edf82d5
tanjanita <szonoko+github@gmail.com>
2021-07-16 16:21:03 +0100

    closes #9 Home: Code responsive layout

--- src/Hero.js
+++ src/Hero.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 function Hero() {
   return (
-    <article className="home">
-
-      <div className="hero">
+    <article className="hero">
+      <div>
         <div className="huge" id="home">Hey, I'm <strong>Tanja</strong>.</div>
         <h1><strong>Front-End Web Developer</strong></h1>
         <p className="subheading1">based in Glasgow.</p>
-        <p className="hero-paragraph">I love to create <strong>clean and structured code</strong> in order to see <strong>great designs come to life</strong>.</p>
-        <p className="subheading3">Welcome to my portfolio!</p>
+        <p className="hero-paragraph">I love to create <strong>clean and structured code</strong> and to see <strong>great designs come to life</strong>.</p>
+        <p className="hero-welcome subheading4">Welcome to my portfolio!</p>
       </div>
-
     </article>
   );
 }
