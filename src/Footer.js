da3208e0e16564a434ad4954a484a243a357a1b3
tanjanita <szonoko+github@gmail.com>
2021-07-22 11:34:18 +0100

    Closes #16 Footer: Code mobile layout

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,30 @@
 function Footer() {
   return (
-    <footer>
-      Footer
+    <footer className="footer">
+
+      <div className="contact">
+        <div className="contact__container">
+          <h3 className="contact__headline">Contact me</h3>
+          <a className="contact__link" href="mailto:riethmueller@mail.com">riethmueller@mail.com</a>
+        </div>
+      </div>
+
+      <div className="footer__navigation">
+        <div className="navigation-links gap">
+          <a className="navigation-links-item" href="#home">Home</a>
+          <a className="navigation-links-item" href="#portfolio">Portfolio</a>
+          <a className="navigation-links-item" href="#skillset">Skillset</a>
+        </div>
+      </div>
+
+      <div className="footer__bar">
+        <p className="footer__copyright">&copy; 2021 Tanja Riethm&uuml;ller</p>
+        <div className="footer__media-links">
+          <a className="footer__media-links-item" href="http://"><i class="fab fa-linkedin"></i></a>
+          <a className="footer__media-links-item" href="http://"><i class="fab fa-github-square"></i></a>
+        </div>
+      </div>
+
     </footer>
   );
 }
