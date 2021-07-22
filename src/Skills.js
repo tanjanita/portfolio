7afbcd20d822b42d10b2e07b45729496980174b9
tanjanita <szonoko+github@gmail.com>
2021-07-22 09:37:05 +0100

    Closes #15 Skills: Code responsive layout

--- src/Skills.js
+++ src/Skills.js
@@ -4,17 +4,19 @@ function Skills() {
   return (
     <article className="skillset">
 
-      <div className="article__header">
+      <div className="article__header skillset-header">
         <h1 className="article__headline" id="skillset">
-        Skillset / About Me
+        Skillset
         </h1>
         <p className="article__description">Reprehenderit esse labore id <strong>veniam</strong> ut veniam <strong>adipisicing amet</strong> non ex ullamco dolor proident. <strong>Exercitation velit</strong> ea incididunt.</p>
+        
+        <p className="article__description">Coursework vs. practical usage & experience.</p>
       </div>
 
       <div className="skillset__collection">
 
         <div className="skillset__category">
-          <h3 className="skillset__category-headline">The Basics</h3>
+          <h4 className="skillset__category-headline">The Basics</h4>
           <ul className="skillset__list">
             <li className="skillset__list-item marker">HTML/5</li>
             <li className="skillset__list-item marker">CSS/3</li>
@@ -28,36 +30,36 @@ function Skills() {
         </div>
 
         <div className="skillset__category">
-          <h3 className="skillset__category-headline">Frameworks<sup>*</sup></h3>
+          <h4 className="skillset__category-headline">Frameworks<sup>*</sup></h4>
           <ul className="skillset__list">
             <li className="skillset__list-item marker">REACT</li>
             <li className="skillset__list-item">jQuery</li>
+            <li className="skillset__list-item">EJS</li>
             <li className="skillset__list-item marker">APIs</li>
             <li className="skillset__list-item marker">RESTful APIs</li>
+            <li className="skillset__list-item">MongoDB</li>
+            <li className="skillset__list-item">Mongoose</li>
             <li className="skillset__list-item marker">Node.js</li>
             <li className="skillset__list-item marker">NPM</li>
             <li className="skillset__list-item marker">Express</li>
-            <li className="skillset__list-item">MongoDB</li>
-            <li className="skillset__list-item">Mongoose</li>
-            <li className="skillset__list-item">EJS view engine</li>
           </ul>
         </div>
 
         <div className="skillset__category">
-          <h3 className="skillset__category-headline">Software<sup>*</sup></h3>
+          <h4 className="skillset__category-headline">Software<sup>*</sup></h4>
           <ul className="skillset__list">
             <li className="skillset__list-item marker">VS Code</li>
             <li className="skillset__list-item marker">GitHub</li>
             <li className="skillset__list-item marker">Terminal/Commandline</li>
-            <li className="skillset__list-item marker">robo3t</li>
-            <li className="skillset__list-item marker">Postman</li>
             <li className="skillset__list-item marker">Figma</li>
-            <li className="skillset__list-item">Codepen</li>
+            <li className="skillset__list-item marker">Codepen</li>
+            <li className="skillset__list-item">robo3t</li>
+            <li className="skillset__list-item">Postman</li>
           </ul>
         </div>
 
         <div className="skillset__category">
-          <h3 className="skillset__category-headline">Conceptual</h3>
+          <h4 className="skillset__category-headline">Conceptual</h4>
           <ul className="skillset__list">
             <li className="skillset__list-item marker">Version control workflow</li>
             <li className="skillset__list-item marker">Problem solving strategies</li>
@@ -67,12 +69,13 @@ function Skills() {
         </div>
 
         <div className="skillset__category">
-          <h3 className="skillset__category-headline">Etcetera</h3>
+          <h4 className="skillset__category-headline">[AFK]</h4>
           <ul className="skillset__list">
-            <li className="skillset__list-item marker">Nature lover</li>
+            <li className="skillset__list-item">Nature explorer</li>
             <li className="skillset__list-item">Japanophile</li>
             <li className="skillset__list-item">Photography dabbler</li>
-            <li className="skillset__list-item marker">Fool for cats ^^ </li>
+            <li className="skillset__list-item">Art lover</li>
+            <li className="skillset__list-item">Fool for cats and all things cute ^._.^ฅ</li>
           </ul>
         </div>
 
