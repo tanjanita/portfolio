ca85649c51af3b18aae844534b3b4ed5a600a763
tanjanita <szonoko+github@gmail.com>
2021-09-21 10:38:17 +0100

    #35 WIP: Added project card images, some content and code updates

--- src/Hero.js
+++ src/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
         <p className="hero__headline" id="home">Hey, I'm <strong>Tanja</strong>.</p>
         <h1 className="hero__title"><strong>Front-End Web Developer</strong></h1>
         <p className="hero__subtitle">based in Glasgow.</p>
-        <p className="hero__introduction">I love to create <strong>clean and structured code</strong> and to see <strong>great designs come to life</strong>.</p>
+        <p className="hero__introduction">I love to create <strong>clear and structured code</strong> and to see <strong>great designs come to life</strong>.</p>
         <p className="hero__welcome">Welcome to my portfolio!</p>
       </div>
     </article>
--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -4,45 +4,45 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function ProjectCard() {
-
   return (
+
     <div className="project">
 
       {Projects.map((project) => {
-
         let oddCard = (project.id % 2 === 0) ? true : false;
-      
-            return (
+        return (
 
-            <div className={`project-card__container${oddCard ? " project-card__container--align-right" : ""}`} key={project.id}>
-              <div className="project-card__off-center-nudge"></div>
-              <div className={`project-card${oddCard ? " project-card--align-right" : ""}`}>
-              <h3 className={`project__headline${oddCard ? " project__headline--align-right" : ""}`}>{project.name}</h3>
-                <p className="project__description">{project.description}</p>
-                <ul className={`horizontal-list${oddCard ? " horizontal-list--align-right" : ""}`}>
+        <div className={`project-card__container${oddCard ? " project-card__container--align-right" : ""}`} key={project.id}>
+          <div className="project-card__off-center-nudge"></div>
+          <div className={`project-card${oddCard ? " project-card--align-right" : ""}`}>
+          <h3 className={`project__headline${oddCard ? " project__headline--align-right" : ""}`}>{project.name}</h3>
+            <p className="project__description">{project.description}</p>
+            <ul className={`horizontal-list${oddCard ? " horizontal-list--align-right" : ""}`}>
 
-                  {project.technologies.map((technology, index) => {
-                    return (
-                      <li key={index} className={`horizontal-list__item${oddCard ? " horizontal-list__item--align-right" : ""}`}>#{technology} </li>
-                    );
-                  })}
+              {project.technologies.map((technology, index) =>  
 
-                </ul>
-                <div className={`project__button-container${oddCard ? " project__button-container--align-right" : ""}`}>
-                  <a className="project__button" href={project.linkPage} target="_blank"  rel="noreferrer" title="Open page in a new tab">
-                    Open the page<span className="screen-reader-only"> for {project.name}</span> 
-                    <FontAwesomeIcon icon={faArrowRight} className="project__button-icon" />
-                  </a>
-                  <a className="project__button alternative-button" href={project.linkCode} target="_blank" rel="noreferrer">
-                    Check out the code<span className="screen-reader-only"> for {project.name}</span> 
-                    <FontAwesomeIcon icon={faArrowRight} className="project__button-icon" />
-                  </a>
-                </div>
-                <div className={`project__image-box${oddCard ? " project__image-box--align-right" : ""}`}> </div>
-              </div>
+                <li key={index} className={`horizontal-list__item${oddCard ? " horizontal-list__item--align-right" : ""}`}>#{technology} </li>
+
+              )}
+
+            </ul>
+            <div className={`project__button-container${oddCard ? " project__button-container--align-right" : ""}`}>
+              <a className="project__button" href={project.linkPage} target="_blank"  rel="noreferrer" title="Open page in a new tab">
+                Open the page<span className="screen-reader-only"> for {project.name}</span> 
+                <FontAwesomeIcon icon={faArrowRight} className="project__button-icon" />
+              </a>
+              <a className="project__button alternative-button" href={project.linkCode} target="_blank" rel="noreferrer">
+                Check out the code<span className="screen-reader-only"> for {project.name}</span> 
+                <FontAwesomeIcon icon={faArrowRight} className="project__button-icon" />
+              </a>
             </div>
+            <div className={`project__image-box${oddCard ? " project__image-box--align-right" : ""}`}>
+              <img src={project.imageURL} className="project__image" alt={`Screenshot of project ${project.name}`}/>
+            </div>
+          </div>
+        </div>
 
-            ); // end of return within map
+        ); // end of return within projects map
       })}
 
     </div>
